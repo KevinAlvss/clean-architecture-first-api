dd0bc06d84e586263e698384e31a84f9d08469b3
KevinAlvss <kevin.bernardo64@gmail.com>
2022-08-15 15:48:04 -0300

    remove duplicated code

--- src/__tests__/login-touter.test.ts
+++ src/__tests__/login-touter.test.ts
@@ -1,6 +1,7 @@
 class LoginController {
   login(httpRequest) {
-    if (!httpRequest.body.email || !httpRequest.body.password) {
+    const { email, password } = httpRequest.body;
+    if (!email || !password) {
       return {
         statusCode: 400,
       };
