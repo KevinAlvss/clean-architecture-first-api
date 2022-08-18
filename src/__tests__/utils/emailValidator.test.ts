3efb75c46f187f447aeee4aa24b0da576e33f01b
KevinAlvss <kevin.bernardo64@gmail.com>
2022-08-18 14:29:37 -0300

    ensure EmailValidator returns false if validator return false

--- src/__tests__/utils/emailValidator.test.ts
+++ src/__tests__/utils/emailValidator.test.ts
@@ -1,14 +1,23 @@
+import validator from "validator";
+
 class EmailValidator {
   isValid(email: string) {
-    return true;
+    return validator.isEmail(email);
   }
 }
 
 describe("Email Validator", () => {
   it("Should return true if validator returns true", () => {
     const sut = new EmailValidator();
-    const isEmailValid = sut.isValid("valid_email#email.com");
+    const isEmailValid = sut.isValid("valid_email@email.com");
 
     expect(isEmailValid).toBe(true);
+  });
+
+  it("Should return false if validator returns false", () => {
+    const sut = new EmailValidator();
+    const isEmailValid = sut.isValid("invalid_email.com");
+
+    expect(isEmailValid).toBe(false);
   });
 });
