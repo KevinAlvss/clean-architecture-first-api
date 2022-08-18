314c1b2dc310648321ad96259c880faf3db815e1
KevinAlvss <kevin.bernardo64@gmail.com>
2022-08-18 14:30:43 -0300

    pass sut to factory function

--- src/__tests__/utils/emailValidator.test.ts
+++ src/__tests__/utils/emailValidator.test.ts
@@ -6,16 +6,20 @@ class EmailValidator {
   }
 }
 
+function makeSut() {
+  return new EmailValidator();
+}
+
 describe("Email Validator", () => {
   it("Should return true if validator returns true", () => {
-    const sut = new EmailValidator();
+    const sut = makeSut();
     const isEmailValid = sut.isValid("valid_email@email.com");
 
     expect(isEmailValid).toBe(true);
   });
 
   it("Should return false if validator returns false", () => {
-    const sut = new EmailValidator();
+    const sut = makeSut();
     const isEmailValid = sut.isValid("invalid_email.com");
 
     expect(isEmailValid).toBe(false);
