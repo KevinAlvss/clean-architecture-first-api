2d6876b6e4bb3c04d2a77ddb818e319af136b252
KevinAlvss <kevin.bernardo64@gmail.com>
2022-08-19 17:29:33 -0300

    injecting a secret in token generator class

--- src/utils/tokenGenerator.test.ts
+++ src/utils/tokenGenerator.test.ts
@@ -1,12 +1,18 @@
 import jwt from "jsonwebtoken";
 class TokenGenerator {
+  secret: string;
+
+  constructor(secret: string) {
+    this.secret = secret;
+  }
+
   async generate(id: string) {
-    return jwt.sign(id, "secret");
+    return jwt.sign(id, this.secret);
   }
 }
 
 function makeSut() {
-  return new TokenGenerator();
+  return new TokenGenerator("my_secret");
 }
 
 describe("Token Generator", () => {
@@ -20,6 +26,6 @@ describe("Token Generator", () => {
   it("Should return a token if JWT returns token", async () => {
     const sut = makeSut();
     const token = await sut.generate("any_id");
-    expect(jwt.verify(token, "secret")).toBe("any_id");
+    expect(jwt.verify(token, sut.secret)).toBe("any_id");
   });
 });
