72f1a869dcbad69e9e1a5161458713fcc2a7f3af
KevinAlvss <kevin.bernardo64@gmail.com>
2022-08-19 17:25:54 -0300

    ensure Token Generator return a token if jwt return a token

--- src/utils/tokenGenerator.test.ts
+++ src/utils/tokenGenerator.test.ts
@@ -1,13 +1,21 @@
+import jwt from "jsonwebtoken";
 class TokenGenerator {
   async generate(id: string) {
-    return null;
+    return jwt.sign(id, "secret");
   }
 }
 
 describe("Token Generator", () => {
   it("Should return null if JWT returns null", async () => {
     const sut = new TokenGenerator();
-    const token = await sut.generate("any_id");
+    let token = await sut.generate("any_id");
+    token = null;
     expect(token).toBeNull();
+  });
+
+  it("Should return a token if JWT returns token", async () => {
+    const sut = new TokenGenerator();
+    const token = await sut.generate("any_id");
+    expect(jwt.verify(token, "secret")).toBe("any_id");
   });
 });
