e0effd535876b5f50a9ac9cd2326072b3ee1a0d4
KevinAlvss <kevin.bernardo64@gmail.com>
2022-08-19 17:11:59 -0300

    refact email validator

--- src/utils/emailValidator.ts
+++ src/utils/emailValidator.ts
@@ -1,18 +1,7 @@
 import validator from "validator";
 
 export class EmailValidator {
-  isEmailValid: boolean;
-  email: string;
-
   isValid(email: string) {
-    this.email = email;
-
-    if (validator.isEmail(email)) {
-      this.isEmailValid = true;
-      return this.isEmailValid;
-    }
-
-    this.isEmailValid = false;
-    return this.isEmailValid;
+    return validator.isEmail(email);
   }
 }
