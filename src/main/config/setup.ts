7426d476a9d72b612e777f003d557adda3224d3b
KevinAlvss <kevin.bernardo64@gmail.com>
2022-08-23 10:55:34 -0300

    enable CORS

--- src/main/config/setup.ts
+++ src/main/config/setup.ts
@@ -2,4 +2,10 @@ import { Express } from "express";
 
 export function setupApp(app: Express) {
   app.disable("x-powered-by");
+  app.use((req, resp, next) => {
+    resp.set("access-control-allow-origin", "*");
+    resp.set("access-control-allow-methods", "*");
+    resp.set("access-control-allow-headers", "*");
+    next();
+  });
 }
