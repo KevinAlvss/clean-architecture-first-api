09b5eb2c40cc2c3239a30ab796edd36f5a4521ef
KevinAlvss <kevin.bernardo64@gmail.com>
2022-08-23 16:53:18 -0300

    enable express.json in setup

--- src/main/config/setup.ts
+++ src/main/config/setup.ts
@@ -1,7 +1,8 @@
-import { Express } from "express";
+import express, { Express } from "express";
 import { cors } from "../middlewares/cors";
 
 export function setupApp(app: Express) {
   app.disable("x-powered-by");
   app.use(cors);
+  app.use(express.json());
 }
